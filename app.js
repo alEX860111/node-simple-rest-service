21975ffe0a5ed107d69749fefe38b68ef1d53195
alEX860111 <alex86011119@gmail.com>
2014-06-01 15:44:12 +0200

    changed from connect to express

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-var connect = require("connect");
-var connectRoute = require("connect-route");
-var app = connect();
+var express = require('express');
+var app = express();
 
-app.use(connect.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
 
-app.use(connect.bodyParser());
+app.get('/test/:name', function(req, res) {
+    res.send('Hello ' + req.params.name);
+});
 
-app.use(connectRoute(function(router) {
-    router.get("/hello/:firstName/:lastName", function(req, res) {
-        var userName = req.params.firstName + " " + req.params.lastName,
-            html = "<!doctype html>" +
-                "<html><head><title>Hello " + userName + "</title></head>" +
-                "<body><h1>Hello, " + userName + "!</h1></body></html>";
-        res.end(html);
-    });
-    router.post("/add", function(req, res) {
-        var x = req.body.x;
-        var y = req.body.y;
-        var result = {
-        	sum: x+y
-        };
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(result));
-    });
-}));
+app.post('/add', function(req, res) {
+    var x = req.body.x;
+    var y = req.body.y;
+    var result = {
+        sum: x + y
+    };
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result));
+});
 
-app.listen(8000);
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
