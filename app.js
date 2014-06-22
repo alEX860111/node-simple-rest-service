27dae80538112643f138343d43c70e203a1cb042
alEX860111 <alex86011119@gmail.com>
2014-06-22 14:30:15 +0200

    removed unused stuff

--- app.js
+++ app.js
@@ -5,20 +5,6 @@ var dao = require('./lib/memdb');
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-app.get('/test/:name', function(req, res) {
-    res.send('Hello ' + req.params.name);
-});
-
-app.post('/add', function(req, res) {
-    var x = req.body.x;
-    var y = req.body.y;
-    var result = {
-        sum: x + y
-    };
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(result));
-});
-
 app.get('/persons', function(req, res) {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(dao.getAll()));
